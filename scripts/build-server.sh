85189ae5768b69e77cd3bff98ed63f23ec8cdb26
patrikx3 <alabard@gmail.com>
2020-03-26 11:52:47 +0100

    robo8x [chore]   3/26/2020, 11:52:47 AM

--- scripts/build-server.sh
+++ scripts/build-server.sh
@@ -10,7 +10,7 @@ set -e
 # some info
 echo
 #echo "Works like command, use a tag: sudo ./scripts/build-server.sh r4.2.0"
-echo "Works like command, use a tag: sudo ./scripts/build-server.sh r4.2.4"
+echo "Works like command, use a tag: sudo ./scripts/build-server.sh r4.2.5"
 echo
 
 # check if we are root
@@ -28,14 +28,14 @@ fi
 
 # require mongo release
 #if [ -z "${2}" ]; then
-#    echo "The second argument must be the MONGODB_RELEASE for example 'r4.2.4'"
+#    echo "The second argument must be the MONGODB_RELEASE for example 'r4.2.5'"
 #    exit 1
 #fi
 #MONGODB_RELEASE="${2}"
 
 # require mongo release
 if [ -z "${1}" ]; then
-    echo "The first argument must be the MONGODB_RELEASE for example 'r4.2.4'"
+    echo "The first argument must be the MONGODB_RELEASE for example 'r4.2.5'"
     exit 1
 fi
 MONGODB_RELEASE="${1}"
@@ -86,7 +86,7 @@ pushd $BUILD
 
         # hack to old version python pip cryptography from 1.7.2 to use the latest
         # sed -i 's#cryptography == 1.7.2#\#cryptography == 1.7.2#g' buildscripts/requirements.txt
-        # this is only because 4.2.4 uses 1.7.2 and
+        # this is only because 4.2.5 uses 1.7.2 and
         # https://github.com/pyca/cryptography/issues/4193#issuecomment-381236459
         # support minimum latest (2.2)
         pip3 install cryptography
